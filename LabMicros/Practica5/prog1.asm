cad7303ac7eef81bc620bf8b8f765cb9e54d67a9
kubos777 <chd.jl@hotmail.com>
2017-03-14 15:46:05 +0000

    Agrego práctica 4 incompleta, falta comentar el código 2 y conclusiones

--- LabMicros/Practica5/prog1.asm
+++ LabMicros/Practica5/prog1.asm
@@ -9,11 +9,11 @@ inicio:
 		bcf STATUS,RP1	
 		movlw H'07'   
 		movwf ADCON1  ;Configuramos el registro como entrada/salida
-		movlw H'FF'   ;
-		movwf TRISA   ;
-		movlw H'00'   
-		movwf TRISB    
-		bcf	STATUS,RP0
+		movlw H'FF'   ;Movemos un 255 a w
+		movwf TRISA   ;Movemos w al registro TRISA
+		movlw H'00'   ;Movemos un 0 a w
+		movwf TRISB    ;Movemos w al registro TRISB
+		bcf	STATUS,RP0 ;Regresamos al banco 0
 AND:
 		movlw H'07'		;Movemos un 7 a w
 		andwf PORTA,w	;Aplicamos un and a lo que hay en el puerto B
